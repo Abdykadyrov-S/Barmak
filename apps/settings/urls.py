55fb3c0cc3201a0b02e46d66f8b0eeee00b6bad5
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-02-26 23:22:13 +0600

    test

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name="index"),
-    path('about', about, name="about"),
+    path('about/', about, name="about"),
     path('contact/', contact, name="contact"),
     path('send_data/', send_data, name="send_data"),
 ]
